c38f0c119ac7b3a751e1de7558ffd4a051020298
Jason Swords <32058619+jasonswords@users.noreply.github.com>
2019-12-03 10:27:52 +0000

    Update embedded_files_in_pdf.sh

--- embedded_files_in_pdf.sh
+++ embedded_files_in_pdf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+pwd=$(pwd)
+echo $pwd
 pdfdetach -saveall "zod.pdf"
-
 count=1000
-count2=1000
 
 while test $count != 0; do
-echo $count
-pdfdetach -saveall "$count".pdf
-count="$(( count - 1 ))"
-if [ $count2 -gt 1 ];then
-rm "$count2".pdf;
-count2="$(( count2 - 1 ))"
-fi
+    echo $count
+    pdfdetach -saveall "$count".pdf
 
+    if [ $count -gt 0 ];then
+        rm "$count".pdf;
+        count="$(( count - 1 ))"
+    else
+        break
+    fi
 done
